099e21668bcc5f65dbc982177deb130b4ce37b5a
d11416148-sys <d11416148@gs.takming.edu.tw>
2026-05-26 16:05:38 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+public interface IGroup
+{
+    public string GetGroup();
+}
+
+public interface IData
+{
+    public string GetData();
+}
+
+public interface IName : IData, IGroup
+{
+    public string GetName();
+}
+
+public class Person : IName
+{
+    public string GetName()
+    {
+        return "Person";
+    }
+
+    public string GetData()
+    {
+        return "Data";
+    }
+    public string GetGroup()
+    {
+        return "Group";
+    }
+}
+
+public class Student : IData
+{
+    public string GetData()
+    {
+        return "Data";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Class_0526_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+
+            IGroup g1 = p1;
+            Console.WriteLine(g1.GetGroup());
+            IData d1 = p1;
+            Console.WriteLine(d1.GetData());
+            IName n1 = p1;
+            Console.WriteLine(n1.GetName());
+            Console.WriteLine(n1.GetGroup());
+            Console.WriteLine(n1.GetData());
+
+            Student s1 = new Student();
+            IData d2 = s1;
+            Console.WriteLine(s1.GetData());
+
+
+        }
+    }
+}
